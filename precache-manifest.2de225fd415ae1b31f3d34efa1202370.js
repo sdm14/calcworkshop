7b018ec16bf0f984886f2999f6d7829bb74230e9
sdm14 <149sdm10@gmail.com>
2020-08-15 19:26:59 +0600

    Updates

---
+++ precache-manifest.2de225fd415ae1b31f3d34efa1202370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ca73164f72587d8f86eba3a33f75eb",
+    "url": "/calcworkshop/index.html"
+  },
+  {
+    "revision": "cd94f5c4cc050fd55dd8",
+    "url": "/calcworkshop/static/css/main.94d4465d.chunk.css"
+  },
+  {
+    "revision": "58029a77ec97e984c1fd",
+    "url": "/calcworkshop/static/js/2.daf2ec05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calcworkshop/static/js/2.daf2ec05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd94f5c4cc050fd55dd8",
+    "url": "/calcworkshop/static/js/main.45cd5025.chunk.js"
+  },
+  {
+    "revision": "fcfe997237b1a96beff4",
+    "url": "/calcworkshop/static/js/runtime-main.41dd25b5.js"
+  }
+]);
